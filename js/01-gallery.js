16305d4d3a4c1e0e29d07248e659a6dab1c6fdbb
Ivan Drahoner <idrahoner@gmail.com>
2022-10-11 20:45:38 +0300

    Completed 1st gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+let originalImage = {};
+
+galleryEl.addEventListener("click", onClickImage);
+
+const galleryCards = galleryItems.map(({ preview, original, description }) => {
+  return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>
+    `;
+});
+
+galleryCards.forEach((element) => {
+  galleryEl.insertAdjacentHTML("beforeend", element);
+});
+
+function onClickImage(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const imageSrc = event.target.dataset.source;
+
+  originalImage = basicLightbox.create(`
+    <img src="${imageSrc}">
+    `);
+
+  originalImage.show();
+  window.addEventListener("keydown", onEscKeyPress);
+}
+
+function onEscKeyPress(event) {
+  if (event.code === "Escape" || event.code === "Space") {
+    originalImage.close();
+    window.removeEventListener("keydown", onEscKeyPress);
+  }
+}
